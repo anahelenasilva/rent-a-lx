d2d6183c627e165d190212d234d321881b472df8
Ana Helena <ahelena-magit@vortx.com.br>
2022-01-30 08:47:35 -0300

    feat: categories WIP

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
 
 app.use(express.json())
+app.use('/categories', categoriesRoutes)
 
 app.get('/', (req, res) => {
     return res.send('Hello World')
