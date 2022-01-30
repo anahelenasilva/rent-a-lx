ae5c98b4c81aeec05aab40d6f448068a282ccd33
Ana Helena <anahelenarp@hotmail.com>
2022-01-30 09:10:38 -0300

    feat: add category repository with create function

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,14 @@
 import { Router } from 'express'
-import { Category } from '../models/category'
+import { CategoryRepository } from '../repositories/categoryRepository'
 
 const categoriesRoutes = Router()
-
-const categories: Category[] = []
+const categoryRepository = new CategoryRepository()
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body
-    const category = new Category()
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
-    categories.push(category)
+    categoryRepository.create({ name, description })
 
-    return response.status(201).json(category)
+    return response.status(201).send()
 })
 
 export { categoriesRoutes }
