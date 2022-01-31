b12fdb9b66a41412494156760c9a09fc5395fb27
Ana Helena <ahelena-magit@vortx.com.br>
2022-01-31 08:22:02 -0300

    feat: add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+
+import * as swaggerDocument from './swagger.json'
 import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(router)
 
 app.get('/', (req, res) => {
